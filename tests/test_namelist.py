bdd11247a19bbfc7b08950610c0f44c67eb5b95a
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2021-06-02 11:10:18 +0100

    test_namelist: Make the Linter happy

--- tests/test_namelist.py
+++ tests/test_namelist.py
@@ -1,8 +1,6 @@
 """
 Tests for handling namelists
 """
-from pathlib import Path
-import pytest
 import f90nml
 from ifsbench import sanitize_namelist, namelist_diff, gettempdir
 
@@ -27,16 +25,16 @@ def test_sanitize_namelist():
     with nml_file.open('w') as f:
         f.write(nml_string)
     nml = f90nml.read(nml_file)
-    
+
     sanitized = sanitize_namelist(nml, merge_strategy='first')
     assert sanitized.todict() == {'a': {'foo': 4, 'foobar': 3}, 'b': {'bar': 1, 'foo': 2}}
-    
+
     sanitized = sanitize_namelist(nml, merge_strategy='last')
     assert sanitized.todict() == {'a': {'foo': 1, 'bar': 2}, 'b': {'bar': 1, 'foo': 2}}
-    
+
     sanitized = sanitize_namelist(nml, merge_strategy='merge_first')
     assert sanitized.todict() == {'a': {'foo': 4, 'foobar': 3, 'bar': 2}, 'b': {'bar': 1, 'foo': 2}}
-    
+
     sanitized = sanitize_namelist(nml, merge_strategy='merge_last')
     assert sanitized.todict() == {'a': {'foo': 1, 'foobar': 3, 'bar': 2}, 'b': {'bar': 1, 'foo': 2}}
 
