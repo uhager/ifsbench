8a40b76f082b264acdcce6cdf593972bc7d8af07
Balthasar Reuter <balthasar.reuter@ecmwf.int>
2021-11-10 12:14:42 +0000

    Modernize Python package setup

--- setup.py
+++ setup.py
@@ -1,23 +1,6 @@
 """
 Install the ifsbench package.
 """
-import os.path
+import setuptools
 
-from setuptools import setup, find_packages
-
-import versioneer
-
-with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as f:
-    long_description = f.read()
-
-setup(
-    name="ifsbench",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="ECMWF",
-    author_email="user_support_section@ecmwf.int",
-    description="IFS benchmark and testing utilities in Python",
-    long_description=long_description,
-    packages=find_packages(),
-    scripts=['scripts/nml-diff.py', 'scripts/ifs-bench.py'],
-)
+setuptools.setup()
