4e3dd4cce0bc6d8adf2632ad6f269a730f076268
Ulrike Hager <ulrike.hager@ecmwf.int>
2025-01-27 12:19:31 +0000

    Trying to fix type error on 3.8

--- ifsbench/config_mixin.py
+++ ifsbench/config_mixin.py
@@ -1,13 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Any, get_args, get_origin, get_type_hints, Union
+from typing import Any, Dict, get_args, get_origin, get_type_hints, List, Union
 
 __all__ = ['ConfigMixin', 'CONF']
 
 
-CONF = Union[int, float, str, bool, dict, list, None]
+CONF = Union[int, float, str, bool, Dict, List, None]
 
 
-def _config_from_locals(config: dict[str, Any]) -> None:
+def _config_from_locals(config: Dict[str, Any]) -> None:
     print(f'from locals: config={config}, type={type(config)}')
     config = config.copy()
     config.pop('self', None)
@@ -35,25 +35,25 @@ class ConfigMixin(ABC):
 
     @classmethod
     @abstractmethod
-    def config_format(cls) -> dict[str, type | dict]:
+    def config_format(cls) -> Dict[str, type | Dict]:
         raise NotImplementedError()
 
     @classmethod
-    def _format_from_init(cls) -> dict[str, type | dict]:
+    def _format_from_init(cls) -> Dict[str, type | Dict]:
         format_definition = dict(get_type_hints(cls.__init__, include_extras=False))
         format_definition = _config_from_locals(format_definition)
         return format_definition
 
-    def set_config_from_init_locals(self, config: dict[str, Any]):
+    def set_config_from_init_locals(self, config: Dict[str, Any]):
         config = _config_from_locals(config)
         self.set_config(config)
 
-    def set_config(self, config: dict[str, CONF]) -> None:
+    def set_config(self, config: Dict[str, CONF]) -> None:
         if self._config:
             raise ValueError('Config already set.')
         self._config = config
 
-    def get_config(self) -> dict[str, CONF]:
+    def get_config(self) -> Dict[str, CONF]:
         return self._config
 
     def update_config(self, field: str, value: CONF) -> None:
@@ -66,13 +66,13 @@ class ConfigMixin(ABC):
         self._config[field] = value
 
     @classmethod
-    def validate_config(cls, config: dict[str, CONF]):
+    def validate_config(cls, config: Dict[str, CONF]):
         format_definition = cls.config_format()
         cls._validate_config_from_format(config, format_definition)
 
     @classmethod
     def _validate_config_from_format(
-        cls, config: dict[str, CONF], format_definition: dict[str, type | dict]
+        cls, config: Dict[str, CONF], format_definition: Dict[str, type | Dict]
     ):
 
         for key, value in config.items():
@@ -87,14 +87,14 @@ class ConfigMixin(ABC):
             if (key not in config) and (type(None) not in get_args(value)):
                 # format_definition key has to be in config unless it's optional
                 raise ValueError(f'"{key}" required but not in {config}')
-            if isinstance(value, dict):
+            if isinstance(value, Dict):
                 # nested, check that field also nested in config, then recursively check dict.
-                if not isinstance(config[key], dict):
+                if not isinstance(config[key], Dict):
                     raise ValueError(
                         f'"{key}" has type {type(config[key])}, expected {value}'
                     )
                 cls._validate_config_from_format(config[key], format_definition[key])
-            elif isinstance(value, list):
+            elif isinstance(value, List):
                 # For now, only check both are lists and first entry type is correct, don't check every entry.
                 if not isinstance(config[key], list):
                     raise ValueError(
